045069689cb4bd2b4f7b4159f0178a9aca6ddf2d
许鹏 <3053949745@qq.com>
2022-10-23 17:58:01 +0800

    基于vitepress的自动化文档/样式体系

---
+++ docs/components/button/index.md
@@ -0,0 +1,11 @@
+# 按钮-Button
+
+:::demo 这是Helloworld组件，我们可以传递msg属性给它
+```vue
+<template>
+<HelloWorld />
+</template>
+```
+:::
+
+<Test />
--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,12 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/Test'
+</script>
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <!-- <HelloWorld msg="Vite + Vue" /> -->
+  <HelloWorld msg="Hello Vue 3 + Vite + Typescript + Vite" />
   <Test />
 </template>
 
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-// import HelloWorld from './components/HelloWorld.vue'
-import Test from './components/Test'
-</script>
 <style scoped>
 .logo {
   height: 6em;

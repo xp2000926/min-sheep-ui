30f1ddb09f7543cd7f3bf06ba3f99d36457da515
许鹏 <3053949745@qq.com>
2022-11-28 17:23:56 +0800

    ci:规范化组件

--- src/button/index.ts
+++ src/button/index.ts
@@ -1,11 +1,13 @@
 import { App } from 'vue'
 import Button from './src/button'
-
+import { installComponent } from '../install'
+import type { SheepUIOptions } from '../_utils/global-config'
 // 具名导出
 export { Button }
 
+// 导出插件
 export default {
-  install(app: App) {
-    app.component(Button.name, Button)
+  install(app: App, options?: SheepUIOptions) {
+    installComponent(app, Button, options)
   }
 }

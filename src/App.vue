3cf4dc375151eb3fefcc9dd4abfc6d1b791602ea
xp2000926 <3053949745@qq.com>
2023-08-24 22:59:20 +0800

    配置 Tailwind CSS

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,9 @@
 <script setup lang="ts">
-// import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 import Test from './components/Test';
 </script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <!-- <HelloWorld msg="Vite + Vue" />Test -->
+  <HelloWorld msg="Hello Vue 3 + Vite + Typescript + Vite" />
   <Test />
 </template>
 
@@ -21,7 +12,6 @@ import Test from './components/Test';
   height: 6em;
   padding: 1.5em;
   will-change: filter;
-  transition: filter 300ms;
 }
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);

d0bf5df1d72e0e86caeb0598b6213e6daf710439
xp2000926 <3053949745@qq.com>
2024-06-16 06:35:18 +0800

    vitepress

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,17 @@
+import Theme from 'vitepress/dist/client/theme-default/index' //默认样式
+import HelloWorld from "../../../src/components/HelloWorld.vue"
+import Test from "../../../src/components/test"
+import "vitepress-theme-demoblock/theme/styles/index.css"
+import DemoBlock from 'vitepress-theme-demoblock/components/DemoBlock.vue'
+import Demo from 'vitepress-theme-demoblock/components/Demo.vue'
+export default {
+    ...Theme,
+    //扩展应用程序实例
+    enhanceApp({ app }) {
+        // 注册组件
+        app.component('HelloWorld', HelloWorld)
+        app.component('Test', Test)
+        app.component('DemoBlock', DemoBlock)
+        app.component('Demo', Demo)
+    }
+}
---
+++ docs/components/button/index.md
@@ -0,0 +1,15 @@
+# 按钮-Button
+
+::: demo 这是HelloWorld 组件，我们可以传递msg 属性给它
+```vue
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</template>
+```
+:::
+:::demo
+```vue
+<Test />
+```
+:::
+
